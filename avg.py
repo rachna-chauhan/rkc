1b5d123ea4e8c61d6c5f4a62552ec8e0648d372e
rachna-chauhan <rachanachauhan.14@gmail.com>
2016-10-20 21:38:30 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,11 @@
+count=0
+sum=0
+avg=0
+while true:
+    num=int(input('enter the no:'))
+    y=input('do you want to contiue(y/n)')
+    count=count+1
+    sum=sum+n
+    if(y=='n'):
+        avg=sum/count
+print('the average is:',avg)
